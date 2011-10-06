f48f4349d287949240945fe34f6c760c81ab46fd
Jacob Swanner <jacob@envylabs.com>
2011-10-06 00:03:13 -0400

    Add express webserver

--- web.js
+++ web.js
@@ -31,3 +31,16 @@ instance.join(config.roomId, function(error, room) {
 //     this.speak("pong!");
 //   });
 // });
+
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
+
+var port = process.env["PORT"] || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
