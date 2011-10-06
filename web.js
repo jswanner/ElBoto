136419bf581c7837c3c8e3e7fcc3d35190456676
Jacob Swanner <jacob@envylabs.com>
2011-10-06 00:28:52 -0400

    Screw express, just make an http server

--- web.js
+++ web.js
@@ -32,15 +32,8 @@ instance.join(config.roomId, function(error, room) {
 //   });
 // });
 
-var express = require('express');
-
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
-
-var port = process.env["PORT"] || 8080;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(process.env["PORT"] || 5000, null);
